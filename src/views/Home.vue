0efa688ef25cfcf66f94755aedd6fc653b9c7c2b
497440287 <57850046+497440287@users.noreply.github.com>
2022-06-30 17:50:38 +0800

    Initialize base

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  div(
+    class="home")
+    img(
+      alt="Vue logo"
+      src="../assets/logo.png")
+    HelloWorld(
+      msg="Welcome to Your Vue.js App")
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      // 参数
+      fromData: {
+        username: '',
+        verifyCode: ''
+      }
+    }
+  }
+}
+
+</script>
